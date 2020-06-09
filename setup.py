b04ab5e670846dcc1a699e3546c3a5e392e3a7ff
Max Zhao <alcasa.mz@gmail.com>
2020-06-10 01:57:54 +0200

    Updating setup py to include libsixel dependency

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(name='matplotlib-sixel',
       version='0.1',
       description='Matplotlib backend for showing sixel graphics',
+      requires=['libsixel-python'],
       author='Markus Gräb',
       author_email='markus.graeb@gmail.com',
       url='https://github.com/koppa/matplotlib-sixel',
